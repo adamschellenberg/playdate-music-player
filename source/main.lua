b15f37fd3265de26796280aafd41100c3feeaff1
Adam Schellenberg <adamschellenberg93@gmail.com>
2023-05-02 20:31:30 -0700

    music plays!

---
+++ source/main.lua
@@ -0,0 +1,55 @@
+import "CoreLibs/object"
+import "CoreLibs/graphics"
+import "CoreLibs/sprites"
+import "CoreLibs/timer"
+
+local gfx <const> = playdate.graphics
+
+local playerSprite = nil
+
+local playerSpeed = 4
+
+local function initialize()
+	local playerImage = gfx.image.new("images/player")
+	playerSprite = gfx.sprite.new(playerImage)
+	playerSprite:moveTo(200, 120)
+	playerSprite:add()
+
+	local backgroundImage = gfx.image.new("images/background")
+	gfx.sprite.setBackgroundDrawingCallback(
+		function(x, y, width, height)
+			gfx.setClipRect(x, y, width, height)
+			backgroundImage:draw(0, 0)
+			gfx.clearClipRect()
+		end
+	)
+
+	music = {}
+	musicChannel = playdate.sound.channel.new()
+	local track, err = playdate.sound.fileplayer.new("sounds/sample", 1)
+	music = track
+	musicChannel:addSource(music)
+	music:setStopOnUnderrun(false)
+	music:play(0)
+	
+
+end
+
+initialize()
+
+function playdate.update()
+	if playdate.buttonIsPressed(playdate.kButtonUp) then
+		playerSprite:moveBy(0, -playerSpeed)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonRight) then
+		playerSprite:moveBy(playerSpeed, 0)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonDown) then
+		playerSprite:moveBy(0, playerSpeed)
+	end
+	if playdate.buttonIsPressed(playdate.kButtonLeft) then
+		playerSprite:moveBy(-playerSpeed, 0)
+	end
+	
+	gfx.sprite.update()
+end
